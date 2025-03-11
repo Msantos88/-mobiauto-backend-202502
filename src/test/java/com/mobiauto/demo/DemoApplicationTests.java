3a646906e201d8d237b42055d1e5f3c63207806e
m.reis.dos.santos <m.reis.dos.santos@avanade.com>
2025-03-11 09:55:36 -0300

    Project created

---
+++ src/test/java/com/mobiauto/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mobiauto.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
